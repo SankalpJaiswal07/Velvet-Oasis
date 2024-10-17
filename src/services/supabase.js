a6b5ed74e51aef489cd149351d0270865e98b570
SankalpJaiswal07 <sankalpj598@gmail.com>
2024-10-17 20:52:37 +0530

    update

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://ltpxzulovhrlxhmhlmnp.supabase.co";
+export const supabaseUrl = "https://izaelzkhdxcifogbojmv.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml6YWVsemtoZHhjaWZvZ2Jvam12Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTUxNjEyNjksImV4cCI6MjAzMDczNzI2OX0.NatVIoYmmaN2JQ92_k0n8uS7ULe0zQweZw_FvCQ2kPs";
 const supabase = createClient(supabaseUrl, supabaseKey);
