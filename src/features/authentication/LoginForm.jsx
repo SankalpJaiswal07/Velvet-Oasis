e5fec74cb325f177676d6404ee88fcd6b35cf5ca
SankalpJaiswal07 <sankalpj598@gmail.com>
2024-10-17 20:37:22 +0530

    update

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -8,7 +8,7 @@ import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
   const [email, setEmail] = useState("sankalp@example.com");
-  const [password, setPassword] = useState("sankalp123");
+  const [password, setPassword] = useState("123456789");
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
